ab2861c68e60f6d1ae2f5f30088f45029ca2b355
PLJNS <me@pljns.com>
2023-10-03 14:24:56 -0400

    2.3.6 build bump

--- src/main.ts
+++ src/main.ts
@@ -86,7 +86,7 @@ async function bootstrap() {
       app,
       new DocumentBuilder()
         .setTitle('MyOrderApp Square API')
-        .setVersion('2.3.5')
+        .setVersion('2.3.6')
         .addBearerAuth()
         .addApiKey(
           { type: 'apiKey', name: config.headerApiKey, in: 'header' },
@@ -123,7 +123,7 @@ async function bootstrap() {
       app,
       new DocumentBuilder()
         .setTitle('MyOrderApp Admin API')
-        .setVersion('2.3.5')
+        .setVersion('2.3.6')
         .addBearerAuth()
         .addApiKey(
           { type: 'apiKey', name: config.headerApiKey, in: 'header' },
