1d403a4a9b57e8d2f47c30110d3f5c1392a82db8
PLJNS <me@pljns.com>
2024-02-01 22:27:24 -0500

    2.5.17 simply a format

--- src/database/migrations/1706833584412-AddIconFileSize.ts
+++ src/database/migrations/1706833584412-AddIconFileSize.ts
@@ -1,14 +1,17 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AddIconFileSize1706833584412 implements MigrationInterface {
-    name = 'AddIconFileSize1706833584412'
+  name = 'AddIconFileSize1706833584412';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "app_config" ADD "iconFileSize" character varying`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "app_config" ADD "iconFileSize" character varying`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "app_config" DROP COLUMN "iconFileSize"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "app_config" DROP COLUMN "iconFileSize"`,
+    );
+  }
 }
